37d304b6acd379bf8174216bdd8ac2e33898afa9
Katarzyna <kataszl768@student.polsl.pl>
2020-10-10 13:28:05 +0200

    array

---
+++ FOCP_lab1/3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+int main ()
+{
+    array <int, 100> data;
+
+    for(int i=0;i<100;i++)
+    {data[i]=i;}
+
+    for(int i=0;i<100;i++)
+    {cout<<data[i]<<endl;}
+
+    return 0;
+}
