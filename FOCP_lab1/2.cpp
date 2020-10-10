2c29a1b91b4ace024ec07aa74996d0390681c1b0
Katarzyna <kataszl768@student.polsl.pl>
2020-10-10 12:40:02 +0200

    loop1

---
+++ FOCP_lab1/2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+main ()
+{
+    int times;
+
+    cout<<"how many times should it be repeated?"<<endl;
+    cin>>times; cout<<endl;
+
+    for (int i=0;i<times;i++)
+    {
+        cout<<"lol"<<endl;
+    }
+    return 0;
+}
