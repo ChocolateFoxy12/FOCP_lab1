e4ea46958901b85c00cc678f0951e9a720d85227
Katarzyna <kataszl768@student.polsl.pl>
2020-10-10 11:55:04 +0200

    my_number w/ if

--- FOCP_lab1/1.cpp
+++ FOCP_lab1/1.cpp
@@ -5,14 +5,18 @@ main ()
 {
     int my_number, messege;
 
-    //cout<<"hello! nice to see you!"<<
+    //cout<<"hello! nice to see you!"<<endl;
 
     cout<<"type your number: ";
     cin>>my_number;
 
     if (my_number==14||my_number==7)
-        {cout<<"nice! :D";}
-        else {cout<<"okay i guess";}
+        {cout<<"nice! :D"<<endl;
+          if (my_number%2==0){cout<<"what an odd number!"<<endl;}
+          else {cout<<"what an even number!"<<endl;}}
+    else {cout<<"okay i guess"<<endl;
+         if (my_number%2==0){cout<<"ur number s odd"<<endl;}
+          else {cout<<"ur number s even"<<endl;}}
 
     return 0;
 }
