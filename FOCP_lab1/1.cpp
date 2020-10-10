edae7b330e2c986c5ad01ebaa04d24a2a0b7a7be
Katarzyna <kataszl768@student.polsl.pl>
2020-10-10 11:45:00 +0200

    my_number

--- FOCP_lab1/1.cpp
+++ FOCP_lab1/1.cpp
@@ -3,7 +3,16 @@
 using namespace std;
 main ()
 {
-    cout<<"hello world!";
+    int my_number, messege;
+
+    //cout<<"hello! nice to see you!"<<
+
+    cout<<"type your number: ";
+    cin>>my_number;
+
+    if (my_number==69||my_number==420)
+        {cout<<"nice! B)";}
+        else {cout<<"meh";}
 
     return 0;
 }
