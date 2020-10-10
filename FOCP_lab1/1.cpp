486859a866382addf317d2c21c8f05e8189c1fe3
Katarzyna <kataszl768@student.polsl.pl>
2020-10-10 11:56:10 +0200

    my_number w/ if

--- FOCP_lab1/1.cpp
+++ FOCP_lab1/1.cpp
@@ -12,11 +12,11 @@ main ()
 
     if (my_number==14||my_number==7)
         {cout<<"nice! :D"<<endl;
-          if (my_number%2==0){cout<<"what an odd number!"<<endl;}
-          else {cout<<"what an even number!"<<endl;}}
+          if (my_number%2==0){cout<<"what an even number!"<<endl;}
+          else {cout<<"what an odd number!"<<endl;}}
     else {cout<<"okay i guess"<<endl;
-         if (my_number%2==0){cout<<"ur number s odd"<<endl;}
-          else {cout<<"ur number s even"<<endl;}}
+         if (my_number%2==0){cout<<"ur number s even"<<endl;}
+          else {cout<<"ur number s odd"<<endl;}}
 
     return 0;
 }
