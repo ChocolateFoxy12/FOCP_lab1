089ab03806ec3a5bb53774ef3ec0fd9aa1c0ae43
Katarzyna <kataszl768@student.polsl.pl>
2020-10-10 12:45:22 +0200

    my_number w/ if and math

--- FOCP_lab1/1.cpp
+++ FOCP_lab1/1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 main ()
 {
     int my_number, messege;
-    float a, b, c;
+    float a, b, c, a1, b1, c1, a2, b2, c2;
 
     cout<<"hello! nice to see you!"<<endl;
 
@@ -21,18 +21,30 @@ main ()
           cout<<endl; cout<<endl;
 
     cout<<"what about some math? please choose your a, b & c!"<<endl;
-    cout<<"a: "; cin>>a; cout<<endl;
-    cout<<"b: "; cin>>b; cout<<endl;
-    cout<<"c: "; cin>>c; cout<<endl;
+    cout<<"a: "; cin>>a;
+    cout<<"b: "; cin>>b;
+    cout<<"c: "; cin>>c;
 
     float x=(c-b)/a;
-  //  float y=
-   // float z=
 
-    cout<<"the x, y & z are: "<<endl;
-    cout<<"x="<<x<<endl;
-   // cout<<"y="<<y<<endl;
-  //  cout<<"z="<<z<<endl;
+    cout<<"ax+b=c <==> x="<<x<<endl;
+
+    cout<<"and now something more! please choose your a1, b1, c1 & a2, b2, c2!"<<endl;
+    cout<<"a1: "; cin>>a1;
+    cout<<"b1: "; cin>>b1;
+    cout<<"c1: "; cin>>c1;
+    cout<<"the second set: "<<endl;
+    cout<<"a2: "; cin>>a2;
+    cout<<"b2: "; cin>>b2;
+    cout<<"c2: "; cin>>c2;
+
+    float x1=(b1*c2-b2*c1)/(b1+a1*b2);
+
+    cout<<"x="<<x1<<endl;
+
+    float y=(c1-a1*x1)/b1;
+
+    cout<<"y="<<y;
 
     return 0;
 }
