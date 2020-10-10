c63f43c39d78cc959d9487484f43ed76e94b5b29
Katarzyna <kataszl768@student.polsl.pl>
2020-10-10 12:06:40 +0200

    my_number w/ if and math

--- FOCP_lab1/1.cpp
+++ FOCP_lab1/1.cpp
@@ -4,10 +4,11 @@ using namespace std;
 main ()
 {
     int my_number, messege;
+    float a, b, c;
 
-    //cout<<"hello! nice to see you!"<<endl;
+    cout<<"hello! nice to see you!"<<endl;
 
-    cout<<"type your number: ";
+    cout<<"please, type your number: ";
     cin>>my_number;
 
     if (my_number==14||my_number==7)
@@ -17,6 +18,21 @@ main ()
     else {cout<<"okay i guess"<<endl;
          if (my_number%2==0){cout<<"ur number s even"<<endl;}
           else {cout<<"ur number s odd"<<endl;}}
+          cout<<endl; cout<<endl;
+
+    cout<<"what about some math? please choose your a, b & c!"<<endl;
+    cout<<"a: "; cin>>a; cout<<endl;
+    cout<<"b: "; cin>>b; cout<<endl;
+    cout<<"c: "; cin>>c; cout<<endl;
+
+    float x=(c-b)/a;
+  //  float y=
+   // float z=
+
+    cout<<"the x, y & z are: "<<endl;
+    cout<<"x="<<x<<endl;
+   // cout<<"y="<<y<<endl;
+  //  cout<<"z="<<z<<endl;
 
     return 0;
 }
